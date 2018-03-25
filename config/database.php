27e467edbbcfb860116705010eb6937acdafffa1
Colin McAtee <colin.g.mcatee@gmail.com>
2018-03-25 17:29:57 -0500

    update provider

--- config/database.php
+++ config/database.php
@@ -1,11 +1,14 @@
 <?php
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
+$bProduction = false;
+if(env('herokustate') === 'production') {
+    $bProduction = true;
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
+}
 
 return [
 
@@ -48,11 +51,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', $host),
+            'host' => env('DB_HOST', $bProduction ? $host : env('DB_HOST')),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', $database),
-            'username' => env('DB_USERNAME', $username),
-            'password' => env('DB_PASSWORD', $password),
+            'database' => env('DB_DATABASE', $bProduction ? $database : env('DB_DATABASE')),
+            'username' => env('DB_USERNAME', $bProduction ? $username : env('DB_USERNAME')),
+            'password' => env('DB_PASSWORD', $bProduction ? $password : env('DB_PASSWORD')),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
