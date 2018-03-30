ca1df01385a199c17cdefb52dce76321f9f016ad
Colin McAtee <colin.g.mcatee@gmail.com>
2018-03-29 22:41:24 -0500

    updates composer

--- config/database.php
+++ config/database.php
@@ -62,11 +62,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', $bProduction ? RDS_HOSTNAME : env('DB_HOST')),
+            'host' => $bProduction ? RDS_HOSTNAME : env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', $bProduction ? RDS_DB_NAME : env('DB_DATABASE')),
-            'username' => env('DB_USERNAME', $bProduction ? RDS_USERNAME : env('DB_USERNAME')),
-            'password' => env('DB_PASSWORD', $bProduction ? RDS_PASSWORD : env('DB_PASSWORD')),
+            'database' => $bProduction ? RDS_DB_NAME : env('DB_DATABASE', 'forge'),
+            'username' => $bProduction ? RDS_USERNAME : env('DB_USERNAME', 'forge'),
+            'password' => $bProduction ? RDS_PASSWORD : env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
