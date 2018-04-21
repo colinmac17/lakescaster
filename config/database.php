09ab9cf277a1aa97fff840c00710fe2a5693fa39
Colin McAtee <colin.g.mcatee@gmail.com>
2018-04-21 10:56:18 -0500

    setting up config for prod and dev

--- config/database.php
+++ config/database.php
@@ -10,7 +10,7 @@
 //    $database = substr($url["path"], 1);
 //}
 
-
+$bProduction = isset($_SERVER['RDS_HOSTNAME']);
 define('RDS_HOSTNAME', $_SERVER['RDS_HOSTNAME']);
 define('RDS_USERNAME', $_SERVER['RDS_USERNAME']);
 define('RDS_PASSWORD', $_SERVER['RDS_PASSWORD']);
@@ -57,11 +57,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => RDS_HOSTNAME,
+            'host' => $bProduction ? RDS_HOSTNAME : env('DB_HOST'),
             'port' => env('DB_PORT', '3306'),
-            'database' => RDS_DB_NAME,
-            'username' => RDS_USERNAME,
-            'password' => RDS_PASSWORD,
+            'database' => $bProduction ? RDS_DB_NAME : env('DB_DATABASE'),
+            'username' => $bProduction ? RDS_USERNAME : env('DB_USERNAME'),
+            'password' => $bProduction ? RDS_PASSWORD : env('DB_PASSWORD'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
