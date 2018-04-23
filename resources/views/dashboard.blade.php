f80d5e63e968dbf29d048fa1b19711201e11698f
Colin McAtee <colin.g.mcatee@gmail.com>
2018-04-22 22:59:58 -0500

    adds in mobile logout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,6 +11,17 @@
                 <div class="card-body">
 
                     You are logged in!
+                    <div id="mobileLogout">
+                        <p>
+                            <a class="btn btn-link text-danger" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                   document.getElementById('logout-form').submit();"
+                            >Logout</a>
+                        </p>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
